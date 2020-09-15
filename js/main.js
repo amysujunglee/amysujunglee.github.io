bad4a53d5a14b65a044391856352674dd4c4d4a1
amysujunglee <amy.sujunglee@gmail.com>
2020-09-15 16:25:00 -0700

    creating a function to update copyright year automatically

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 // Filterable portfolio - jQuery
-$(document).ready(function () {
-    $(".filter-button").click(function () {
-        var value = $(this).attr("data-filter");
-        if (value == "all") {
-            $(".filter").show("1000");
-        } else {
-            $(".filter").not("." + value).hide("1000");
-            $(".filter").filter("." + value).show("1000");
-        }
-    });
-    //Add active class
-    $("ul .filter-button").click(function () {
-        $(this).addClass('filter-active').siblings().removeClass('filter-active');
-    })
-})
+$(document).ready(function() {
+  $(".filter-button").click(function() {
+    var value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".filter").show("1000");
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide("1000");
+      $(".filter")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+  //Add active class
+  $("ul .filter-button").click(function() {
+    $(this)
+      .addClass("filter-active")
+      .siblings()
+      .removeClass("filter-active");
+  });
+});
+
+// Update copyright year automatically
+const copyrightYear = document.querySelector("#copyright-year");
+
+copyrightYear.innerHTML = new Date().getFullYear();
